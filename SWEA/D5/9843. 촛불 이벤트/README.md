ee839536b197bdec5c24d4b87d6d0852a433435d
SilverStar <98890934+thisiseunjin@users.noreply.github.com>
2023-07-01 11:01:19 +0900

    [D5] Title: 촛불 이벤트, Time: 1,283 ms, Memory: 127,932 KB -BaekjoonHub

---
+++ SWEA/D5/9843. 촛불 이벤트/README.md
@@ -0,0 +1,11 @@
+# [D5] 촛불 이벤트 - 9843 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXGBKzuaPOoDFAXR) 
+
+### 성능 요약
+
+메모리: 127,932 KB, 시간: 1,283 ms, 코드길이: 1,237 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D5/9843. 촛불 이벤트/촛불 이벤트.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    static int T;
+    //static StringBuilder sb = new StringBuilder();
+    static long N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+
+        for (int test = 1; test < T + 1; test++) {
+            //sb.append("#" + test + " ");
+            N = Long.parseLong(br.readLine());
+
+            long left = 1;
+            long right = 10000000000L;
+            long result = 0;
+
+            while (left <= right) {
+                long mid = (left + right) / 2;
+                long val = mid * (mid + 1) / 2;
+
+                if (N >= val) {
+                    //값이 될 확률이 있다. 늘려준다.
+                    result = Math.max(mid, result);
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            long val = result * (result + 1) / 2;
+            if (N != val) {
+                result = -1;
+            }
+
+            System.out.printf("#%d %d\n", test, result);
+        }
+
+
+    }
+}
