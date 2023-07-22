abe30477156026a01d4fa488541de8cfa747b741
SilverStar <98890934+thisiseunjin@users.noreply.github.com>
2023-07-22 23:02:27 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    static String str;
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+        System.out.println(str+"??!");
+    }
+
+}
