35f2a95898cf3cc2e81048f4ae4c44e6e41516c5
SilverStar <98890934+thisiseunjin@users.noreply.github.com>
2023-07-25 23:38:05 +0900

    [Silver I] Title: 1, 2, 3 더하기 4, Time: 372 ms, Memory: 155336 KB -BaekjoonHub

---
+++ 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0) {
+            int n = sc.nextInt();
+            int[][] dp = new int[10001][4];
+            dp[1][1] = 1;
+            dp[2][1] = 1;
+            dp[2][2] = 1;
+            dp[3][1] = 1;
+            dp[3][2] = 1;
+            dp[3][3] = 1;
+            for (int i = 4; i <= n; i++) {
+                dp[i][1] = dp[i-1][1];
+                dp[i][2] = dp[i-2][1] + dp[i-2][2];
+                dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+            }
+            System.out.println(dp[n][1] + dp[n][2] + dp[n][3]);
+        }
+
+    }
+}
