76f93dd1bea9c97cb5aee74898530cba0bf4ecf0
SilverStar <98890934+thisiseunjin@users.noreply.github.com>
2023-11-07 20:53:48 +0900

    [level 3] Title: 네트워크, Time: 0.11 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,58 @@
+class Solution {
+    static int[] parents;
+    static boolean[] isChecked;
+    static int result=0;
+    
+    public int solution(int n, int[][] computers) {
+        parents = new int[n];
+        isChecked = new boolean[n];
+        
+        //초기값은 자기 자신이다.
+        for(int i=0;i<n;i++){
+            parents[i] = i;
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                //i와 연결된 친구들임!
+                if(i==j) continue;
+                if(computers[i][j]==1) union(i,j);
+                
+            }
+        }
+        
+        
+        for(int i=0;i<n;i++){
+            int p = find(i);
+            if(isChecked[p]) continue;
+            isChecked[p] = true;
+            result++;
+        }
+        
+        
+        return result;
+    }
+    
+    public int find(int a){
+        //부모 찾기 시작~!
+        
+        if(a==parents[a]) return a;
+        
+        //계속 부모를 찾으려고 올라간다.
+        parents[a] = find(parents[a]);
+        return parents[a];
+    }
+    
+    public void union(int a, int b){
+        
+        int rootA = parents[a];
+        int rootB = parents[b];
+        
+        if(rootA == rootB) return;  //둘을 같은 노드에 있다.
+        
+        //작은 쪽이 부모가 된다.
+        if(rootA>rootB){
+            parents[b] = rootA;
+        }else parents[a] = rootB;
+    }
+}
