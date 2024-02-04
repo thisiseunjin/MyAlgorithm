308b1dc7e4108ca71c503de473c5fbe70d224e2f
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2024-02-04 22:56:19 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -3,19 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int result = 0;
-		while (N >= 0) {
-			if (N % 5 == 0) {
-				result += (N / 5);
-				System.out.println(result);
-				return;
-			}
-			N -= 3;
-			result++;
-		}
-		System.out.println(-1);
-	}
+    static int N;
+    static int result=0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        //상근이는 5kg, 3kg가져갈 수 있음.
+        while (N>=0){
+            if(N%5==0){
+                result+=(N/5);
+                System.out.println(result);
+                return;
+            }
+
+            N-=3;
+            result++;
+        }
+
+        System.out.println(-1);
+    }
 }
