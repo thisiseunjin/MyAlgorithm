8f21301f37ba1bcba4e8ba77bb5aaffdec561211
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2024-03-14 00:35:25 +0900

    [level 3] Title: 풍선 터트리기, Time: 23.49 ms, Memory: 133 MB -BaekjoonHub

---
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    
+    static int N;
+    public int solution(int[] a) {
+        int answer = 2;
+        N = a.length;
+        if(N<3) return N;
+        
+        
+        int[] left = new int[N];
+        int[] right = new int[N];
+        
+        int min  = a[0];
+        //내기준 왼쪽 최소값 저장
+        for(int i=1;i<N-1;i++){
+            left[i] = min;
+            // min = Math.min(a[i], min);
+             if(a[i] < min){
+                min = a[i];
+            }
+        }
+        
+        //내기준 오른쪽 최소값 저장
+        min = a[N-1];
+        for(int i=N-2;i>0;i--){
+            right[i]=min;
+            // min = Math.min(min, a[i]);
+             if(a[i] < min){
+                min = a[i];
+            }
+        }
+        
+        for(int i=1;i<N-1;i++){
+            if(left[i]<a[i] && right[i]<a[i]) continue;
+            answer++;
+            // System.out.println(answer);
+        }
+
+        return answer;
+    }
+}
