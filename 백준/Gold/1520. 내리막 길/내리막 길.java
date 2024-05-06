0c18364b1dfa4e9434cb6f50e5864d0c62092076
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2024-05-06 20:16:22 +0900

    [Gold III] Title: 내리막 길, Time: 320 ms, Memory: 38924 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[][] map;
+    static int[][] dp;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        map = new int[M][N];
+        dp = new int[M][N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
+            }
+        }
+
+
+        System.out.println(dfs(0,0));
+
+    }
+
+    public static int dfs(int x, int y) {
+
+        if (x == M - 1 && y == N - 1) {
+            return 1;
+        }
+        if (dp[x][y] != -1) return dp[x][y];
+
+        dp[x][y] = 0;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (!inRange(nx, ny)) continue;
+            if (map[nx][ny] >= map[x][y]) continue;
+
+            dp[x][y] += dfs(nx, ny);
+        }
+        return dp[x][y];
+
+    }
+
+    public static boolean inRange(int x, int y) {
+        if (x < 0 || y < 0 || x > M - 1 || y > N - 1) return false;
+        return true;
+    }
+
+    public static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
+}
