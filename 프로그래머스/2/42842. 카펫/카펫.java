b9965512ba468e59485dfcc0fdcc8ae6d8920010
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2024-05-29 23:06:04 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        
+        int[] answer = new int[2];
+        int sum = brown + yellow;
+        
+        for(int i=3;i<sum;i++){
+            int j = sum/i;
+            
+            if(sum%i>0) continue;
+            if(j<3) continue;
+            
+            int col = Math.max(i,j);
+            int row = Math.min(i,j);
+            
+            int center = (col -2) * (row-2);
+            
+            if(center == yellow){
+                answer[0] = col;
+                answer[1] = row;
+                
+                return answer;
+            }
+        }
+        
+        
+      
+        return answer;
+    }
+}
