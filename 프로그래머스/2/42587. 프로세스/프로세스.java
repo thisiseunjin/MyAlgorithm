2a09e32d42dffc1c884b275833405cb599052603
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2024-07-21 22:33:58 +0900

    [level 2] Title: 프로세스, Time: 2.27 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -8,7 +8,6 @@ class Solution {
             pq.add(i);
         }
         
-        
         Queue<Integer> q = new ArrayDeque<>();
         for(int i=0;i<priorities.length;i++){
             q.add(i);
@@ -17,24 +16,20 @@ class Solution {
         int turn=0;
         while(!q.isEmpty()){
             int idx = q.poll();
-            
             //내가 뽑은 친구의 우선순위는?
             int priority = priorities[idx];
-            
             //내가 뽑은게 우선순위가 높따?
             if(pq.peek()==priority){
                 pq.poll();
-                order[idx] = turn++;
+                order[idx] = ++turn;
                 continue;
             }
-            
             //내가 뽑은게 우선순위가 낮따?
             //그럼 다시 넣지 뭥,,,
             q.add(idx);
         }
-        
-        System.out.println(Arrays.toString(order));
-        return order[location]+1;
+
+        return order[location];
         
     }
 }
