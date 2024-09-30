72d3b44a97de82c7868ef7645cf01da9a971e87d
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2024-09-30 12:40:02 +0900

    [level 3] Title: 단어 변환, Time: 0.59 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,65 +1,65 @@
 import java.util.*;
-
 class Solution {
-    static int result;
-    static class Word{
-        String w;
-        int depth;
-        
-        public Word(String w, int depth){
-            this.w = w;
-            this.depth = depth;
-        }
-    }
+    int result=(int)1e9;
+    boolean[] isVisited;
+    int N;
     public int solution(String begin, String target, String[] words) {
+        isVisited = new boolean[words.length];
+        N = words.length;
         
         bfs(begin, target, words);
-            
-        return result;
+        
+        return result==(int)1e9?0:result;
     }
     
     public void bfs(String begin, String target, String[] words){
-        boolean[] isVisited = new boolean[words.length];
         Queue<Word> q = new ArrayDeque<>();
         q.add(new Word(begin, 0));
         
         while(!q.isEmpty()){
             Word word = q.poll();
             
-            if(word.w.equals(target)){
-                result = word.depth;
-                return;
+            if(target.equals(word.str)){
+                result = Math.min(word.depth, result);
             }
             
-            for(int i=0;i<words.length;i++){
-                if(isVisited[i]) continue;
-                if(!isPossible(word.w, words[i])) continue;
+            for(int i=0;i<N;i++){
+                if(isVisited[i]) continue;  //이미 바꾼 경험이 있으면? 안간다.
+                if(!isPossible(word.str, words[i])) continue;
+
                 isVisited[i] = true;
                 q.add(new Word(words[i], word.depth+1));
             }
             
         }
+    }
+    
+    class Word{
+        String str;
+        int depth;
         
+        public Word(String str, int depth){
+            this.str = str;
+            this.depth = depth;
+        }
     }
     
     public boolean isPossible(String str, String target){
-        
-        //가능한 조건
-        //1. 한번에 한개의 알파벳만 바꿀 수 있다.
-        //2. words에 있는 단어로반 변환할 수 있다.
-        
-        int diff = 0;
-        for(int i=0;i<target.length();i++){
-            char t = target.charAt(i);
+        //1개의 알파벳만 바꿀 수 있다. -> str와 target의 차가 한글자여야한다.
+        //모든 딘어의 길이는 같다.
+        int diffCount=0;    //다른 글자의 개수
+        for(int i=0;i<str.length();i++){
+            char c1 = str.charAt(i);
+            char c2 = target.charAt(i);
             
-            char s = str.charAt(i);
-            
-            if(t!=s) diff++;
-            
-            if(diff==2) return false;
+            if(c1==c2) continue;
+            if(diffCount>0) return false;
+            diffCount++;           
         }
         
-        return true;
-    
+        if(diffCount==1) return true;
+        return false;
+        
+
     }
 }
